ff3f376f92d3a8ab3cf92e18185cd2a53db52580
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-06 00:59:01 +0000

    jj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-/*Show a line where
- *show some
- *word
- */
+/*Show a line where show some word*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
