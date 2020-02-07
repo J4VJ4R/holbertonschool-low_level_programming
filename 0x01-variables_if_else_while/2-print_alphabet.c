4336e2d3de18d5ebf069466586918a48e9199c06
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-07 00:55:00 +0000

    mm

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - last digit
+ * - A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+char letter = 'a';
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
