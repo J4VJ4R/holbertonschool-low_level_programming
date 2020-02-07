995f42c9aca3f6edd90c3cfacdd405ccb27a8e71
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-07 23:00:51 +0000

    jjj

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - last digit
+ * - A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(48 + i);
+if (i < 9)
+{
+putchar(44);
+}
+putchar(32);
+i++;
+}
+putchar('\n');
+return (0);
+}
