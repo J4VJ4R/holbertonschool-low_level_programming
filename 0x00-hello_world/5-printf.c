f6048ec55311500e6d663acd8075813c751e3837
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-09 18:57:29 +0000

    show a line of string with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include<stdio.h>
+/**
+ *main - print a string with printf
+ *
+ *Return: Always o (succes)
+ */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
