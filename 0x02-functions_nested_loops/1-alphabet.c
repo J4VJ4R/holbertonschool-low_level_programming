d505611be3d6717dfabe5f8398702943e2116296
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-11 07:04:44 +0000

    Write a function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 on success
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter < 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	
+}
