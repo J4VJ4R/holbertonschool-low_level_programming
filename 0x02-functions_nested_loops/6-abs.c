c5ecab13fe8a6b83d2894361c2442031b97b71a0
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-11 13:40:05 +0000

    fix the error when update the files dont exist

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _abs - show the absolute value of n
+ * @n:int checked
+ * Return: 0 on success
+ */
+
+int _abs(int n)
+{
+	int abso;
+
+	abso = abs(n);
+	return (abso);
+}
