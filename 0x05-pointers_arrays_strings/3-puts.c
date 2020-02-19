d2ab4e5dfdb44733a06bb0465b04e4889d386786
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-19 00:13:41 +0000

    a function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str:char
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar (str[a]);
+
+	}
+	_putchar('\n');
+}
