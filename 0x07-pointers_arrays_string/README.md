c28f8d972195c46abb101e35129f6adf5c9fd6b2
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-24 19:30:55 +0000

    A function that fill a pointer

---
+++ 0x07-pointers_arrays_string/README.md
@@ -0,0 +1,3 @@
+#POINTERS AND DIMENSIONAL ARRAYS#
+We learning more pointers and use them in other case also
+bidemsional arrays
---
+++ 0x07-pointers_arrays_string/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+char *_memset(char *s, char b, unsigned int n);
+#endif /* _SAMPLE_HEADER_H_ */
