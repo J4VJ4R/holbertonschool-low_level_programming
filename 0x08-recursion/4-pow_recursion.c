2eea2aad32990ce8f926ec9e1801f93bc44f0809
JavierJARA <ingenierodesoftware73@gmail.com>
2020-02-27 00:07:31 +0000

    a function that print the result of potencia

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,9 @@
 
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _pow_recursion - check the code for Holberton School students.
+ * @x:integer
+ * @y:integer
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
