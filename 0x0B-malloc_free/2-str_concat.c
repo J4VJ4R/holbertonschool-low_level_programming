a50467ea95fe42b02773e664e67014554cdafee4
JavierJARA <ingenierodesoftware73@gmail.com>
2020-03-04 12:59:34 +0000

    addin again functin for other string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,12 +16,22 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int l;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	if (s1 == NULL)
+		i = 0;
+	else
 	{
+		for (i = 0; s1[i] != '\0'; i++)
+			;
 	}
 	i--;
-	for (j = 0; s2[j] != '\0'; j++)
+	if (s2 == NULL)
 	{
+		j = 0;
+	}
+	else
+	{
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 	}
 	size = i + j;
 
