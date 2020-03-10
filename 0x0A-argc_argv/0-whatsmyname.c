6c9ea9ac0f962ef50cf4850dcfba6f7f317431c5
JavierJARA <ingenierodesoftware73@gmail.com>
2020-03-10 15:59:35 +0000

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - check the code for Holberton School students.
+ * @s: char
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
