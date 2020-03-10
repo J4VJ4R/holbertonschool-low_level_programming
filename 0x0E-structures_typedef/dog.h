8324d4b9eafb7fdb38c9b2892251591c152b6fb4
JavierJARA <ingenierodesoftware73@gmail.com>
2020-03-10 01:14:00 +0000

    defining for second time a structure tomy dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - creating a structure for a dog
+ *@name: name of the dog
+ *@owner: name of the owner
+ *@age: age of the dog
+ */
 struct dog
 {
 	char *name;
