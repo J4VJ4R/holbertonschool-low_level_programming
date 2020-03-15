e9c4849b343aef103d560d0ea19969e05ce8eae8
Javier <ingenierodesoftware73@gmail.com>
2020-03-15 01:04:39 +0000

    this is a fix for the checker

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef VAR_F
 #define VAR_F
-int sum_them_all(const unsigned int n, ...);
+#include <stdarg.h>
+typedef struct formats
+{
+	char *format_module;
+	void (*f)(va_list list);
+} op_t;
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
