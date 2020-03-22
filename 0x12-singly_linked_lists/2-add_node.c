db529ba99a69621046c16c5091c6a7386f939aae
Javier Jaramillo <1604@holbertonschool.com>
2020-03-22 21:59:52 +0000

    this is a fix to add node in a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,44 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int _strlen(const char *s);
 /**
- * print_list - check the code for Holberton School students.
- *  @h: this is a list
+ * *add_node - check the code for Holberton School students.
+ *  @head: this is a list
+ *  @str: this is a list
  * Return: a size_t
  */
+
+
 list_t *add_node(list_t **head, const char *str)
 {
-    unsigned int len = 0;
-    list_t *nuevo_nodo, *tmp;
-    nuevo_nodo = (list_t *) malloc(sizeof(list_t));
+list_t *nuevoPersonaje;
 
-    if (nuevo_nodo != NULL)
-    {
-        nuevo_nodo->len = len;
-        nuevo_nodo->str = strdup(str);
-        nuevo_nodo->next = *head;
-    }
-
-    if (*head == NULL)
-    {
-        *head = nuevo_nodo;
-    }
-    else
-    {
-        /*agregar el nodo de último*/
-        tmp = *head;
-        while (tmp->next != NULL)
-        {
-            /*PROCESO para recorrer la lista*/
-            tmp = tmp->next;
-        }
-        /*cuando lo anterior sea falso nos salimos del while y tendriamos en tmp el último elemento*/
-        /*el último nodo de la lista*/
-        tmp->next = nuevo_nodo; /*este ya no apuntaría a null sino que va a ser igual al nuevo nodo*/
-        /*que nosotros estamos agregando que ya previamente está escrito en la linea 18 y terminemos de*/
-        /*enlazar la lista*/
-        
-    }
-    return (nuevo_nodo);
-    
+nuevoPersonaje = malloc(sizeof(list_t));
+nuevoPersonaje->len = _strlen(str);
+nuevoPersonaje->next = *head;
+nuevoPersonaje->str = strdup(str);
+*head = nuevoPersonaje;
+if (head == NULL)
+{
+*head = nuevoPersonaje;
+}
+else
+{
+}
+return (nuevoPersonaje);
+}
+/**
+ * _strlen - this a function to counter the characters
+ * of the string
+ * @s: character pointer
+ * Return: return value of i
+ */
+int _strlen(const char *s)
+{
+int i;
+while (*(s + i))
+{
+i++;
+}
+return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,11 +11,15 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct list_s
+typedef struct list_s/*definimos la estructura*/
 {
-char *str;
-unsigned int len;
-struct list_s *next;
+    char *str; /*primer valor de la estructura tipo char*/
+    unsigned int len; /*segundo valor de la estructura tipo unsigned int*/
+    struct list_s *next; /*la autoreferenciamos con struct list_s*//*variable apuntador*/
+    /**la variable apuntador next de tipo struct list_s nos sirve para unir una estructura
+    * de tipo list_s con otra del mismo tipo, este es el vínculo para ver si existe más nodos o más
+    * elementos en nuestra lista
+    */
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
