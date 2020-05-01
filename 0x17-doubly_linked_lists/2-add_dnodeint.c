0841c57836ce3fd161941464c4b72098580f0953
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-01 15:26:22 -0500

    different changes and creatting folder test

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * dlistint_len - a function to show the number of elements
+ * @h: variable of the elements inside
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp;
+
+    tmp = *head;
+}
---
+++ tests/listaDoble.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <conio.h>
+
+typedef struct nodo
+{
+    int dato;
+    struct nodo* siguiente;
+    struct nodo* atras;
+}nodo;
+
+nodo* primero = NULL;
+nodo* ultimo =  NULL;
+
+void insertarNodo();
+
+void desplegarListaPU();
+void desplegarListaUP();
+int main(){
+    return (0);
+}
+
+// lista de 13, 16, 2, 9
+
+// primero == 13 ultimo = 9 nuevo = 9
+
+//lista doble ----------- null <- 13 -> <- 16 -> <- 2 -> <- 9 -> NULL
+void insertarNodo(){
+    nodo* nuevo = (nodo*)malloc(sizeof(nodo));
+    print(" ingresa el dato que contendrá el nuevo nodo: ");
+    scanf("%d", &nuevo->dato);
+
+    if(primero == NULL){
+        primero = nuevo;
+        primero->siguiente = NULL;
+        primero->atras = NULL;
+        ultimo = primero;
+    }else{
+        ultimo->siguiente = nuevo;
+        nuevo->siguiente = NULL;
+        nuevo->atras = ultimo;
+        ultimo = nuevo
+    }
+
+    printf("\n Nodo ingresado con éxito\n\n")
+}
+
+void desplegarListaPU(){
+    nodo* actual = (nodo)malloc(sizeof(nodo));
+    actual = primero;
+    if(primero != NULL){
+        while (actual != NULL)
+        {
+            printf("\n %d", actual->dato)
+            actual = actual->siguiente;
+        }
+        
+    }else{
+        printf("\nlas lista se encuentra vacía\n\n");
+    }
+}
+
+void desplegarListaUP(){
+    nodo* actual = (nodo)malloc(sizeof(nodo));
+    actual = ultimo;
+    if(primero != NULL){
+        while (actual != NULL)
+        {
+            printf("\n %d", actual->dato)
+            actual = actual->atras;
+        }
+        
+    }else{
+        printf("\nlas lista se encuentra vacía\n\n");
+    }
+}
