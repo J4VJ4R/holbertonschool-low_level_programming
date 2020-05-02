5aa3145beb9167d4206c3302b0b231516ce8bef0
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-02 09:54:17 -0500

    I forget the prototype

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 /**
  * 
@@ -7,19 +5,19 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *nuevo;
+	dlistint_t *nuevo;
 
-    nuevo = *head;
-    if (head == NULL)
-        return (NULL);
-    nuevo = malloc(sizeof(dlistint_t));
-    if (nuevo == NULL)
-        return (NULL);
-    nuevo->n = n;
-    nuevo->prev = NULL;
-    nuevo->next = *head;
-    if (*head != NULL)
-        (*head)->next = nuevo;
-    *head = nuevo;
-    return (nuevo);
+	nuevo = *head;
+	if (head == NULL)
+		return (NULL);
+	nuevo = malloc(sizeof(dlistint_t));
+	if (nuevo == NULL)
+		return (NULL);
+	nuevo->n = n;
+	nuevo->prev = NULL;
+	nuevo->next = *head;
+	if (*head != NULL)
+		(*head)->next = nuevo;
+	*head = nuevo;
+	return (nuevo);
 }
