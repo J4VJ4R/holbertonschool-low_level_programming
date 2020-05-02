ac0b3fa84574df0cc581277d24efb98c2772fe6f
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-01 20:20:53 -0500

    beautiful code please is for betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,24 +3,25 @@
  * add_dnodeint - a function to show the number of elements
  * @head: variable of the elements inside
  * @n: cuantiti of the element
+ * return: a temp
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *tmp;
 
-    tmp = *head;
-    if (head == NULL)
-        return (NULL);
-    tmp = malloc(sizeof(dlistint_t));
-    if (tmp == NULL)
-        return (NULL);
-    tmp->n = n;
-    tmp->prev = NULL;
-    tmp->next = *head;
+	tmp = *head;
+	if (head == NULL)
+		return (NULL);
+	tmp = malloc(sizeof(dlistint_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->n = n;
+	tmp->prev = NULL;
+	tmp->next = *head;
 
-    if (*head != NULL)
-        (*head)->prev = tmp;
-    *head = tmp;
-    return (tmp);    
+	if (*head != NULL)
+		(*head)->prev = tmp;
+	*head = tmp;
+	return (tmp);
 }
