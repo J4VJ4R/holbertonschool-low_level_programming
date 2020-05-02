bb1c877bde04b83b0d6e8051c3013dcb01024438
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-01 20:14:18 -0500

    add a new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
- * dlistint_len - a function to show the number of elements
- * @h: variable of the elements inside
+ * add_dnodeint - a function to show the number of elements
+ * @head: variable of the elements inside
+ * @n: cuantiti of the element
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -9,4 +10,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *tmp;
 
     tmp = *head;
+    if (head == NULL)
+        return (NULL);
+    tmp = malloc(sizeof(dlistint_t));
+    if (tmp == NULL)
+        return (NULL);
+    tmp->n = n;
+    tmp->prev = NULL;
+    tmp->next = *head;
+
+    if (*head != NULL)
+        (*head)->prev = tmp;
+    *head = tmp;
+    return (tmp);    
 }
