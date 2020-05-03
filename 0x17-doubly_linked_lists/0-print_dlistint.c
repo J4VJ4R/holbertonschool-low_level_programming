095e4cc2080948e11a2d481ba68ac4d924be7ef2
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 15:07:32 -0500

    add a description of Return and prototype and indentation

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,15 +2,16 @@
 /**
  * print_dlistint - a function to show the number of elements
  * @h: variable of the elements inside
+ * Return: a count variable
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; h; i++)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
-    return (i);
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
