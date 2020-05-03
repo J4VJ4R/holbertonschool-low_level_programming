2da3e0d0ca18065750916f0bb190d0145fb2f1c2
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 15:15:58 -0500

    add a description to return

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,6 +2,7 @@
 /**
  * dlistint_len - a function to show the number of elements
  * @h: variable of the elements inside
+ * Return: a variable to count the number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
