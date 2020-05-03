42b606a70f88ede888d4ffc9f6cb1e5219ef8470
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 18:39:45 -0500

    a function that returns the nth node of a dlistint_t linked list.

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint - is a function to add a new node at the end
+ * @head: where give a new space
+ * @index: is a variable where inside the number of space or of
+ * element
+ */
+*get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *nodo;
+    nodo = *head;
+    if(nodo == NULL)
+        return;
+    for (i = 0; head; i++)
+    {
+        if (i == index){
+            printf("%d". nodo->n);
+        }
+    }
+    
+}
