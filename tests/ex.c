81d11d9450350691e16f84c313909b2cf9bd1042
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-04 04:10:17 -0500

    function sum

--- tests/ex.c
+++ tests/ex.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 
 void holamundo(){
-    printf("hola holberton");
+    printf("hola holberton\n");
+}
+
+int suma(int a, int b){
+    return (a + b);
 }
 
 int main(void)
 {
    holamundo();
+   int val = suma(1, 2);
+   printf("la suma es: %d", val);
     return 0;
 }
