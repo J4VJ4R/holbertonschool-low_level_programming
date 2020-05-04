74793a1101c108998865b7402bee01811da89321
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-04 04:02:21 -0500

    make owner libraries

--- tests/libreria1.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LIBRERIA_1_H
-#define _LIBERARIA_1_H
-#include <stdlib.h>
-#include "libpropia.a"
-int suma (int a, int b);
-int resta (int a, int b);
-#endif
--- tests/suma.c
+++
@@ -1,7 +0,0 @@
-#include "libreria1.h"
-
-int main(){
-    int a;
-    a = suma(5, 5);
-    printf("%d\n", a);
-}
