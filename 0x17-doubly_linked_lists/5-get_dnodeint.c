c7984018cac31c175abdb751fd1d1e450fae7e28
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 19:10:40 -0500

    mix in description of a function

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_dnodeint_at_index(dlistint_t - is a function to add
+ * get_dnodeint_at_index - is a function to add
  * a new node at the end
  * @head: where give a new space
  * @index: is a variable where inside the number of space or of
