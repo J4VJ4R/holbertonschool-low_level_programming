03423112fdb21cfd109f7d42605ac926761c9e11
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 19:06:12 -0500

    indentation and description of function

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * free_dlistint - is a function to add a new node at the end
+ *  get_dnodeint_at_index(dlistint_t - is a function to add a new node at the end
  * @head: where give a new space
  * @index: is a variable where inside the number of space or of
  * element
  * Return: the head
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t(*head, unsigned int index)
 {
-    if(head == NULL)
+    if (head == NULL)
         return (NULL);
     while (index != 0 && head != NULL)
     {
