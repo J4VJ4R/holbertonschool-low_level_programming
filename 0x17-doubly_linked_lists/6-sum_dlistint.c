9149ade5888335334fb5bd37038fe3a9542296cc
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 20:58:10 -0500

    betty is beotufull

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -8,16 +8,14 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-    dlistint_t *list = head;
-    int count = 0;
-    if (head == NULL)
-        return (0);
-    while (list != NULL)
-    {
-        count += list->n;
-        list = list->next;
-        
-    }
-    return (count);
-    
+	dlistint_t *list = head;
+	int count = 0;
+	if (head == NULL)
+		return (0);
+	while (list != NULL)
+	{
+		count += list->n;
+		list = list->next;
+	}
+	return (count);
 }
