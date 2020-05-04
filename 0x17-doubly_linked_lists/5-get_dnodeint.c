95a13cd9d4dea399c46445b4768be09b6460bdc6
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-03 19:08:47 -0500

    more of 80 characters in line 3

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- *  get_dnodeint_at_index(dlistint_t - is a function to add a new node at the end
+ * get_dnodeint_at_index(dlistint_t - is a function to add
+ * a new node at the end
  * @head: where give a new space
  * @index: is a variable where inside the number of space or of
  * element
