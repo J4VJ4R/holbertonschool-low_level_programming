eda3ac6737eff4d4686d2eeec8f7457923d60cd6
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-29 16:45:47 -0500

    no double =

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(actual->key, key) == 0)
 		{
 			free(actual->value);
-			actual->value == strdup(value);
+			actual->value = strdup(value);
 			if (actual->value == NULL)
 	            return (0);
 			return (1);
