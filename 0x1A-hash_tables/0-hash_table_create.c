cf2f0b51af919b749b2dd40efc337096de76ef70
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-29 15:26:20 -0500

    betty fock my life

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,20 +6,21 @@
  * Return: a hash table
  */
 
-hash_table_t *hash_table_create(unsigned long int size){
-    hash_table_t *hashTable;
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashTable;
 
-    if (size == 0)
-        return (NULL);
-    hashTable = calloc(1, sizeof(hash_table_t));
-    if (hashTable == NULL)
-        return (NULL);
-    hashTable->size = size;
-    hashTable->array = calloc(size, sizeof(hash_node_t **));
-    if (hashTable->array == NULL)
-    {
-        free(hashTable);
-        return (NULL);
-    }
-    return (hashTable);
+	if (size == 0)
+		return (NULL);
+	hashTable = calloc(1, sizeof(hash_table_t));
+	if (hashTable == NULL)
+		return (NULL);
+	hashTable->size = size;
+	hashTable->array = calloc(size, sizeof(hash_node_t **));
+	if (hashTable->array == NULL)
+	{
+		free(hashTable);
+		return (NULL);
+	}
+	return (hashTable);
 }
