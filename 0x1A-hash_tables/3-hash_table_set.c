5378ed30077e2e4e37f1445aad9e749462330bff
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-05-29 16:47:19 -0500

    suspect code indent

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(actual->value);
 			actual->value = strdup(value);
 			if (actual->value == NULL)
-	            return (0);
+				return (0);
 			return (1);
 		}
 		actual = actual->next;
